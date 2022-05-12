fdd37a87af44c0a803a415599911707c5b16e2c0
gabriel.vogado <g28@12r95v>
2022-05-12 04:53:57 -0300

    Tratamento de Exception Status Code 400 JediNotFound

---
+++ src/main/java/br/com/spring/study/springwebmvc/rest/ResourceAdvice.java
@@ -0,0 +1,16 @@
+package br.com.spring.study.springwebmvc.rest;
+
+import br.com.spring.study.springwebmvc.exception.JediNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ResourceAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(JediNotFoundException.class)
+    public void notFound () {
+    }
+}
